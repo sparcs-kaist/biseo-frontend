bd5ae67ad359feaa3e0175610b56454c64594aa4
pacokwon <haechank@gmail.com>
2020-05-28 12:51:44 +0000

    add: redirect to appropriate routes after login

--- src/components/LoginCallback.js
+++ src/components/LoginCallback.js
@@ -1,21 +1,33 @@
-import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useLocation, Redirect } from 'react-router-dom'
 import querystring from 'querystring'
 import axios from '../utils/axios'
+import { saveToken } from '../utils/auth'
 
 const LoginCallback = () => {
   const location = useLocation()
   const query = location.search
   const { code, state } = querystring.parse(query[0] === '?' ? query.slice(1) : query)
+  const [valid, setValid] = useState(null)
 
   useEffect(() => {
     axios.get(`/account/login/callback?code=${code}&state=${state}`)
-      .then(res => {
-        console.log(res.data)
+      .then(({ data }) => {
+        if (data.token) {
+          saveToken(data.token)
+          setValid(true)
+        } else {
+          setValid(false)
+        }
       })
   }, [])
 
-  return <div>{ `${code}-${state}` }</div>
+  if (valid === null)
+    return <div>Loading...</div>
+  else if (valid)
+    return <Redirect to='/dashboard'/>
+  else
+    return <Redirect to='/login'/>
 }
 
 export default LoginCallback
