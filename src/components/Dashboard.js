145cfa0c2b1f3d8b2d372ff707b0693952d36e40
pacokwon <haechank@gmail.com>
2020-05-28 12:53:25 +0000

    add: mock Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { logout } from '../utils/auth'
+
+const Dashboard = () => {
+  const history = useHistory()
+
+  const handleLogout = () => {
+    logout()
+    history.replace('/login')
+  }
+
+  return (
+    <div>
+      <h1>Hello, Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
+}
+
+export default Dashboard
