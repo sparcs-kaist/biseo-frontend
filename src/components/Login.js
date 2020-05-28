66f6b624e7259aa05858ea1b5447fd979daaca60
pacokwon <haechank@gmail.com>
2020-05-28 12:52:07 +0000

    add: redirect from login page to to dashboard if already logged in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 import './Login.css'
+import axios from '../utils/axios'
 
-const Login = () => (
-  <div id='login'>
-    <a href='/login/redirect'>LOGIN</a>
-  </div>
-)
+const Login = () => {
+  const [loggedIn, setLoggedIn] = useState(null)
 
+  useEffect(() => {
+    axios.get('/auth')
+      .then(({ data }) => {
+        setLoggedIn(data.success)
+      })
+  }, [])
+
+
+  if (loggedIn)
+    return <Redirect to='/dashboard'/>
+
+  return (
+    <div id='login'>
+      <a href='/login/redirect'>LOGIN</a>
+    </div>
+  )
+}
 
 export default Login
