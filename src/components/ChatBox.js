3a81b917450c30969c7ee01ba8cc42840ae2bec9
pacokwon <haechank@gmail.com>
2020-05-29 17:25:09 +0900

    update: chat refactoring

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,151 +1,45 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react'
 import './ChatBox.css'
 
-function ChatBox(props) {
-  // chatting_height = "50%" // 채팅방 크기 (전체 html 태그 기준 * 70% * 현재 퍼센트가 채팅방 높이가 됨)
-  const chattings = useRef(null)
-  const [prev_time] = useState([0,0,0,0,0,0])
-  const set_prev_time = (new_one) => {
-    for(let i = 0; i < 6; i++){
-      prev_time[i] = new_one[i]
+const MessageTypes = {
+  NEW: 'new',
+  MEMBERS: 'members',
+  MESSAGE: 'message',
+  OUT: 'out'
+}
+
+const Message = ({ message }) => {
+  const justification = message.type === MessageTypes.MESSAGE ?
+    ('issuer' in message ? 'flex-start' : 'flex-end') :
+    (message.type === MessageTypes.NEW || message.type === MessageTypes.OUT ? 'space-around' : '')
+
+  const getContent = () => {
+    switch (message.type) {
+      case MessageTypes.MESSAGE:
+        if ('issuer' in message)
+          return `${message.issuer}: ${message.payload}`
+        else
+          return `Me: ${message.payload}`
+      case MessageTypes.NEW:
+        return `${message.payload} has entered`
+      case MessageTypes.OUT:
+        return `${message.payload} has left`
     }
   }
-  const _check= (d) => { // check 함수는 도배 방지용으로 0.1초 안에 보낸 메시지가 있으면 자동으로 block 시킴 -> Mac 키보드 오류도 방지함
-    let time =[d.getYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(),d.getMilliseconds()]
-    const interval = 100
-    for(let i = 0; i < 6 ; i++){
-      if(time[i]>prev_time[i]){
-        console.log("a")
-        set_prev_time(time)
-        return true
-      }
-    }
-    if(time[6]>prev_time[6]+interval){
-      console.log("b")
-      set_prev_time(time)
-      return true
-    }
-    console.log("blocked")
-    return false
-  }
-
-  const _send_click = () => { // click 버튼 들어오면 chat message 로 socket에 메시지 보냄
-    let d = new Date();
-    if(_check(d)){ // 도배 방지 체크
-      const message = document.getElementById("message").value
-
-      props.socket.emit('chat message',message)
-      const li = document.createElement('li')
-      li.className = "mine"
-      li.innerText = "Me: " + message
-      document.getElementById("message").value = ""
-      chattings.current.appendChild(li)
-      chattings.current.scrollTop = chattings.current.scrollHeight // 채팅 스크롤 맨 아래로 유지
-
-
-    }
-  }
-
-  const _send_enter = (event) => { // enter 버튼 들어오면 chat message 로 socket에 메시지 보냄
-    if (event.keyCode === 13) {
-      let d = new Date();
-
-      if(_check(d)){ // 도배 방지 체크
-        event.preventDefault();
-        const message = document.getElementById("message").value
-        props.socket.emit('chat message',message)
-        const li = document.createElement('li')
-        li.className = "mine"
-        li.innerText = "Me: " + message
-        document.getElementById("message").value = ""
-        chattings.current.appendChild(li)
-
-        chattings.current.scrollTop = chattings.current.scrollHeight // 채팅 스크롤 맨 아래로 유지
-
-      }
-    }
-  };
-
-  const _preprocess = () => {
-    // const chatting_height = "50%" // 채팅방 크기 (전체 html 태그 기준 * 70% * 현재 퍼센트가 채팅방 높이가 됨)
-    // chattings.style.height = chatting_height
-
-    document.getElementById("message").addEventListener("keyup", _send_enter)
-    document.getElementById("send").addEventListener("click", _send_click)
-    console.log(chattings.current)
-    props.socket.on('members',function(accessor){ // 변경된 members 정보를 socket 으로부터 받아서 div.members에 넣어줌
-      console.log(accessor)
-
-      if(document.getElementsByClassName("members")[0].childElementCount !== 0){
-        console.log("일단")
-        document.getElementsByClassName("members")[0].innerHTML = ""
-      }
-      for (let i=0 ; i<accessor.length; i++){
-        const li = document.createElement('li')
-        li.innerText = accessor[i]
-        document.getElementsByClassName("members")[0].appendChild(li)
-      }
-    })
-
-    props.socket.on('chat message',function(user, message){ // chat message를 socket 으로부터 받아서 ul.chatting 에 li child 로 넣어줌
-      const li = document.createElement('li')
-      li.innerText = user + ": " + message
-      li.className = "recieved"
-      chattings.current.appendChild(li)
-
-      chattings.current.scrollTop = chattings.current.scrollHeight // 채팅 스크롤 맨 아래로 유지
-
-    })
-
-    props.socket.on('enter',function(username){ // 새로운 유저가 들어왔다는 것을 socket으로부터 받아서 ul.chatting 에 보어줌
-
-      const li = document.createElement('li')
-      li.className = "enter"
-      li.innerText = username + " is entered"
-      console.log("user add")
-      chattings.current.appendChild(li)
-
-      chattings.current.scrollTop = chattings.current.scrollHeight // 채팅 스크롤 맨 아래로 유지
-
-    })
-
-    props.socket.on('out',function(username){ // 유저가 나갔다는 것을 socket으로부터 받아서 ul.chatting 에 보어줌
-
-      const li = document.createElement('li')
-      li.className = "enter"
-      li.innerText = username + " left the room"
-      console.log("user left")
-      chattings.current.appendChild(li)
-
-      chattings.current.scrollTop = chattings.current.scrollHeight // 채팅 스크롤 맨 아래로 유지
-
-    })
-
-    props.socket.on('name',function(name){ // 로그인이 없는 상태이므로 접속하면 랜덤 유저이름을 socket에서 보내주고 그걸 받아서 보여줌
-      document.getElementById("username").innerText = "Your name is " + name
-    })
-  }
-
-
-  useEffect(()=>{
-    _preprocess()
-  }, [])
-
   return (
-    <div className="content">
-      <div className="aside">
-        <ul className="members">
-        </ul>
-      </div>
-      <div className="display">
-        <ul className="chatting" ref={chattings}></ul>
-        <input type="text" id="message"/>
-        <button id="send">Send</button>
-        <button id="below">아래로</button>
-      </div>
+    <div style={{ display: 'flex', justifyContent: justification}}>
+      {getContent()}
     </div>
   )
-
 }
+
+const ChatBox = ({ chatlog }) => {
+  return (
+    <div className="chatbox">
+      {chatlog.map((chat, idx) => <Message key={idx} message={chat} />)}
+    </div>
+  )
+}
+
 
 export default ChatBox
--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,20 +1,69 @@
-import React from 'react'
-import socketio from 'socket.io-client'
+import React, { useState, useEffect, useMemo } from 'react'
 import ChatBox from './ChatBox'
+import socketio from 'socket.io-client'
 import { getToken } from '../utils/auth'
-import './ChatPage.css'
+
+const MessageTypes = {
+  NEW: 'new',
+  MEMBERS: 'members',
+  MESSAGE: 'message',
+  OUT: 'out'
+}
 
 const ChatPage = () => {
-  const socket = socketio.connect('kong.sparcs.org:32811', { query: `token=${getToken()}`})
+  const socket = useMemo(() => socketio.connect(
+    'kong.sparcs.org:32811',
+    {
+      transports: ['websocket'],
+      upgrade: false,
+      query: `token=${getToken()}`
+    }
+  ), [])
+  const [name, setName] = useState('')
+  const [chatlog, setChatlog] = useState([]) // elements of chatlog have two required fields: type, payload
+  const [members, setMembers] = useState([])
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    socket.on('name', username => setName(username))
+    socket.on('enter', username => {
+      setChatlog(chatlog => [{ type: MessageTypes.NEW, payload: username }, ...chatlog])
+      setMembers(members => [...members, username])
+    })
+    socket.on('members', members => setMembers(members))
+    socket.on('chat message', (user, message) => {
+      setChatlog(chatlog => [{ type: MessageTypes.MESSAGE, payload: message, issuer: user }, ...chatlog])
+    })
+    socket.on('out', username => {
+      setChatlog(chatlog => [{ type: MessageTypes.OUT, payload: username }, ...chatlog])
+      setMembers(members => members.filter(member => member !== username))
+    })
+  }, [])
+
+  const handleMessageChange = e => { setMessage(e.target.value) }
+
+  const sendMessage = () => {
+    setMessage('')
+    socket.emit('chat message', message)
+    setChatlog([{ type: MessageTypes.MESSAGE, payload: message }, ...chatlog])
+  }
+
+  const handleMessageKeypress = e => { if (e.key === 'Enter') sendMessage() }
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Chatting room</h1>
-        <h2 id="username"></h2>
+    <>
+      <h3>Hello, {name}</h3>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+        {members.map(member => <div key={member}>{member}</div>)}
+        </div>
+        <div style={{ flexGrow: 10 }}>
+          <ChatBox chatlog={chatlog} />
+          <input type="text" value={message} onChange={handleMessageChange} onKeyPress={handleMessageKeypress}/>
+          <button onClick={sendMessage}>Send</button>
+        </div>
       </div>
-      <ChatBox socket={socket} />
-    </div>
+    </>
   )
 }
 
