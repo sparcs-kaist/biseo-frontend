d60a373a5e30bd47e11c6cfb75f3b03241366743
pacokwon <haechank@gmail.com>
2020-10-09 14:03:10 +0000

    update: refactor Login component with styled components

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import './Login.css';
-import axios from '../utils/axios';
+import styled from 'styled-components';
+import axios from '@/utils/axios';
+
+const Container = styled.div`
+  -moz-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  -webkit-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 4px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  height: 345px;
+  justify-content: space-evenly;
+  margin: 0 auto;
+  margin-top: 100px;
+  width: 440px;
+`;
+
+const HeaderGroup = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainHeader = styled.h1`
+  margin: 0 0 15px 0;
+`;
+
+const SubHeader = styled.h4`
+  color: #f2a024;
+  margin: 0 0 15px 0;
+`;
+
+const RedirectAnchor = styled.a`
+  background-color: #f2a024;
+  border-radius: 13px;
+  color: white;
+  display: inline-block;
+  padding: 10px 20px;
+  text-decoration: none !important;
+`;
 
 const Login: React.FC = () => {
   const [loggedIn, setLoggedIn] = useState(null);
@@ -15,13 +56,13 @@ const Login: React.FC = () => {
   if (loggedIn) return <Redirect to="/dashboard" />;
 
   return (
-    <div id="biseo-login">
-      <div>
-        <h1>BISEO</h1>
-        <h4>Login To Enter</h4>
-      </div>
-      <a href="/login/redirect">LOGIN</a>
-    </div>
+    <Container>
+      <HeaderGroup>
+        <MainHeader>BISEO</MainHeader>
+        <SubHeader>Login To Enter</SubHeader>
+      </HeaderGroup>
+      <RedirectAnchor href="/login/redirect">LOGIN</RedirectAnchor>
+    </Container>
   );
 };
 
