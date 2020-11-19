e5799fb04a1c2430f592a5426c7141a7e87169b1
pacokwon <haechank@gmail.com>
2020-11-19 11:03:18 +0000

    add: temporary mock data for Main component

---
+++ src/pages/Main/mock.ts
@@ -0,0 +1,36 @@
+export const mockTabs = [
+  {
+    title: 'Foo',
+    choices: ['A', 'B', 'C'],
+    extendableChoices: true
+  },
+  {
+    title: 'Bar',
+    choices: ['D', 'E', 'F'],
+    extendableChoices: false
+  },
+  {
+    title: 'Baz',
+    choices: ['D', 'E'],
+    extendableChoices: false
+  }
+];
+// const title = 'SPARCS 종강총회 디자이너 - 최다은';
+// const content =
+//   'SPARCS 종강총회 안건 중 "디자이너"를 투표하기 위한 설문조사입니다.\n각 안건이 진행될 당시 "활동중인 정회원"만이 투표권을 갖습니다.\n투표 결과는 집계가 완료되면 총회 진행중에 공지하도록 하겠습니다.';
+// const subtitle = '최다은(cheddar) 회원의 디자이너 자격 획득에 대한 투표';
+
+export const mockVoteItems = [
+  {
+    active: true,
+    content: 'SPARCS 종강총회 개발자 - 홍길동'
+  },
+  {
+    acitve: false,
+    content: 'SPARCS 종강총회 개발자 - 스팍스'
+  },
+  {
+    active: false,
+    content: 'SPARCS 종강총회 개발자 - 스비서'
+  }
+];
