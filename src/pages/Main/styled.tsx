af3a388db6e1cc192585396c2d4b578c6fe84ed6
pacokwon <haechank@gmail.com>
2020-11-20 17:16:40 +0000

    wip: apply grid style for main page

--- src/pages/Main/styled.tsx
+++ src/pages/Main/styled.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { device, size } from '@/utils/device';
 
-export const MainContainer = styled.div`
+export const UserMainContainer = styled.div`
   display: grid;
   grid-template-areas:
     'chat'
@@ -9,14 +9,80 @@ export const MainContainer = styled.div`
   grid-template-rows: 60vh auto;
   column-gap: 30px;
   row-gap: 30px;
+  margin: 0 8vh;
+
+  & > .vote-items {
+    grid-area: vote-items;
+    display: flex;
+    flex-direction: column;
+    gap: 10px 0;
+    align-self: start;
+  }
+
+  & > .chat {
+    grid-area: chat;
+    min-height: 90vh;
+  }
+
+  & > .admin {
+    grid-area: admin;
+  }
 
   @media ${device.laptop} {
     grid-template-columns: 3fr 4fr;
-    grid-template-rows: 90vh auto;
+    grid-template-rows: 90vh;
     grid-template-areas: 'vote-items chat';
   }
 
   @media ${device.laptopL} {
+    max-width: ${size.laptopL};
+    margin: 0 auto;
+  }
+`;
+
+export const AdminMainContainer = styled.div`
+  display: grid;
+  grid-template-areas:
+    'chat'
+    'admin'
+    'vote-items';
+  grid-template-rows: 60vh auto auto;
+  column-gap: 30px;
+  row-gap: 30px;
+  margin: 0 8vh;
+
+  & > .vote-items {
+    grid-area: vote-items;
+    display: flex;
+    flex-direction: column;
+    gap: 10px 0;
+    align-self: start;
+  }
+
+  & > .chat {
+    grid-area: chat;
+    min-height: 90vh;
+  }
+
+  & > .admin {
+    grid-area: admin;
+  }
+
+  @media ${device.laptop} {
+    grid-template-columns: 3fr 4fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      'admin      chat'
+      'vote-items chat';
+  }
+
+  @media ${device.laptopL} {
+    grid-template-columns: 3fr 4fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      'admin      chat'
+      'vote-items chat';
+
     max-width: ${size.laptopL};
     margin: 0 auto;
   }
