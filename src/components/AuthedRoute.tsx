f992a38aaa6a22f7cc3e75e6e6fbe435c066e0e3
pacokwon <haechank@gmail.com>
2021-03-31 13:42:21 +0900

    refactor(useAuth): authentication check logic in hook

--- src/components/AuthedRoute.tsx
+++ src/components/AuthedRoute.tsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
-import { checkLoginStatus } from '@/utils/auth';
+import { useAuth } from '@/hooks';
+import { LoginStatus } from '@/common/enums';
 
 const AuthedRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
-  const [authed, setAuthed] = useState(null);
+  const loginStatus = useAuth();
 
-  useEffect(() => {
-    checkLoginStatus()
-      .then(isLoggedIn => setAuthed(isLoggedIn))
-      .catch(() => setAuthed(false));
-  }, []);
-
-  if (authed === null) return <div>Loading...</div>;
+  if (loginStatus === LoginStatus.Pending) return <div>Loading...</div>;
   else
     return (
-      <Route {...rest}>{authed ? children : <Redirect to="/login" />}</Route>
+      <Route {...rest}>
+        {loginStatus === LoginStatus.LoggedIn ? (
+          children
+        ) : (
+          <Redirect to="/login" />
+        )}
+      </Route>
     );
 };
 
