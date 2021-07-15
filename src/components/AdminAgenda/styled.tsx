ac246abd5e89f37a6aa91c96d731c70d84440925
Seowan Lee <seowan.lee@bagelcode.com>
2021-07-15 21:52:10 +0900

    <feat>(styled):change style of content

--- src/components/AdminAgenda/styled.tsx
+++ src/components/AdminAgenda/styled.tsx
@@ -30,11 +30,26 @@ export const AgendaContent = styled.div`
 `;
 
 export const DetailContainer = styled.div`
-  background: #fdfbee;
-  border-radius: 10px;
-  max-width: 30%;
+  align-items: center;
+  background: #ffffff;
+  border-bottom-left-radius: 10px;
+  border-top-left-radius: 10px;
   box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.25);
-  padding: 30px;
+  font-weight: 700;
+  padding: 20px 30px;
+  position: relative;
+
+  &::before {
+    background: #f2a024;
+    border-bottom-left-radius: 5px;
+    border-top-left-radius: 5px;
+    content: '';
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 5px;
+  }
 `;
 
 export const DetailContainerTitle = styled.div`
