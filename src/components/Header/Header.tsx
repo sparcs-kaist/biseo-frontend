193ef04955c1aceded220d2124e4bce5ced5903b
pacokwon <haechank@gmail.com>
2021-08-19 22:56:56 +0900

    feat: handle header login button click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import Logo from '@/public/sparcs.svg';
-import BiseoButton from '@/components/BiseoButton';
-import { COLOR } from '@/common/style';
+import { useHistory } from 'react-router-dom';
 import { MdSettings, MdAccountCircle } from 'react-icons/md';
+import BiseoButton from '@/components/BiseoButton';
+import { useTypedSelector, useTypedDispatch } from '@/hooks';
+import { logout as logoutAction } from '@/store/slices/login';
+import Logo from '@/public/sparcs.svg';
+import { setUser } from '@/store/slices/user';
+import { COLOR } from '@/common/style';
+import { logout } from '@/utils/auth';
 import { HeaderContainer, HeaderContent, RHS, OptionButton } from './styled';
 
 const Header: React.FC = () => {
+  const history = useHistory();
+  const isLoggedIn = useTypedSelector(state => state.loggedIn);
+  const dispatch = useTypedDispatch();
+
+  const handleClick = () => {
+    if (isLoggedIn) {
+      logout();
+      dispatch(logoutAction());
+      dispatch(setUser(null));
+    }
+    history.replace('/login');
+  };
+
   return (
     <HeaderContainer>
       <HeaderContent>
         <Logo style={{ height: 'var(--header-height)' }} />
         <RHS>
-          <BiseoButton background={COLOR.primary} foreground="white">
-            login
+          <BiseoButton
+            onClick={handleClick}
+            background={COLOR.primary}
+            foreground="white"
+          >
+            {isLoggedIn ? 'logout' : 'login'}
           </BiseoButton>
           <OptionButton>
             <MdSettings size="24px" />
