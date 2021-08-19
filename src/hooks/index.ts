fe0870fa3321fda64d3140920a46bcfd7ebb7031
pacokwon <haechank@gmail.com>
2021-08-19 22:56:56 +0900

    feat: handle header login button click

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,24 +1,34 @@
 import { useState, useEffect } from 'react';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { logout, login } from '@/store/slices/login';
+import { setUser } from '@/store/slices/user';
 import { LoginStatus } from '@/common/enums';
 import { checkLoginStatus } from '@/utils/auth';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState } from '@/common/types';
 import type { AppDispatch } from '@/store';
-
-export const useAuth = (): LoginStatus => {
-  const [loggedIn, setLoggedIn] = useState<LoginStatus>(LoginStatus.Pending);
-
-  useEffect(() => {
-    checkLoginStatus()
-      .then(isLoggedIn =>
-        setLoggedIn(isLoggedIn ? LoginStatus.LoggedIn : LoginStatus.NotLoggedIn)
-      )
-      .catch(() => setLoggedIn(LoginStatus.NotLoggedIn));
-  });
-
-  return loggedIn;
-};
 
 // eslint-disable-next-line
 export const useTypedDispatch = () => useDispatch<AppDispatch>();
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAuth = (): LoginStatus => {
+  const [loggedIn, setLoggedIn] = useState<LoginStatus>(LoginStatus.Pending);
+  const dispatch = useTypedDispatch();
+
+  useEffect(() => {
+    checkLoginStatus()
+      .then(isLoggedIn => {
+        setLoggedIn(
+          isLoggedIn ? LoginStatus.LoggedIn : LoginStatus.NotLoggedIn
+        );
+        dispatch(login());
+      })
+      .catch(() => {
+        setLoggedIn(LoginStatus.NotLoggedIn);
+        dispatch(logout());
+        dispatch(setUser(null));
+      });
+  });
+
+  return loggedIn;
+};
