36dc7a341f1aadacda43627723aed302d876a553
kjy2844 <kjy2844@kaist.ac.kr>
2022-01-05 21:35:18 +0900

    fix(main-page): delete legacy code

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,14 +2,11 @@ import React, { useState, useEffect, useMemo } from 'react';
 import io from 'socket.io-client';
 import { Agenda } from '@/common/types';
 import { AgendaStatus } from '@/common/enums';
-import AdminBoard from '@/components/AdminBoard';
-import AdminAgenda from '@/components/AdminAgenda';
 import ChatBox from '@/components/ChatBox';
 import UserAgenda from '@/components/UserAgenda';
 import { getToken } from '@/utils/auth';
 import axios from '@/utils/axios';
-import { mockTabs } from '../AdminPage/mock';
-import { AdminMainContainer, UserMainContainer } from './styled';
+import { UserMainContainer } from './styled';
 import { Redirect } from 'react-router-dom';
 
 interface CommonMainProps {
@@ -31,46 +28,6 @@ const UserMain: React.FC<CommonMainProps> = ({ socket, agendas }) => {
         <ChatBox socket={socket} />
       </div>
     </UserMainContainer>
-  );
-};
-
-const AdminMain: React.FC<CommonMainProps> = ({ socket, agendas }) => {
-  const [isEdit, setIsEdit] = useState<boolean>(false);
-  const [targetAgenda, setTargetAgenda] = useState<Agenda>();
-
-  const onEdit = (_id: string) => {
-    if (!isEdit) {
-      setTargetAgenda(agendas.find(agenda => agenda._id == _id));
-    }
-    setIsEdit(!isEdit);
-  };
-
-  const confirmEdit = (_id: string) => {
-    setIsEdit(false);
-  };
-
-  return (
-    <AdminMainContainer>
-      <div className="admin">
-        <AdminBoard
-          socket={socket}
-          tabs={mockTabs}
-          isEdit={isEdit}
-          targetAgenda={targetAgenda}
-          confirmEdit={confirmEdit}
-        />
-      </div>
-      <div className="agendas">
-        {agendas.map(item => (
-          <AdminAgenda
-            key={item._id}
-            socket={socket}
-            onEdit={onEdit}
-            {...item}
-          />
-        ))}
-      </div>
-    </AdminMainContainer>
   );
 };
 
