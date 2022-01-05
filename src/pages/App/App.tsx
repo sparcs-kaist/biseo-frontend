2425a8338e9b8c24518ac881d2e6d3a2061846be
kjy2844 <kjy2844@kaist.ac.kr>
2022-01-05 21:35:49 +0900

    feat(admin-page): only admin can access admin page

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GlobalStyle } from '@/common/style';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import AuthedRoute from '@/components/AuthedRoute';
+import { AuthedRoute, AdminAuthedRoute } from '@/components/AuthedRoute';
 import {
   Login,
   LoginRedirect,
@@ -21,12 +21,6 @@ const App: React.FC = () => {
       <Header />
       <AppContainer>
         <Switch>
-          <Route exact path="/admin">
-            <AdminPage />
-          </Route>
-          <Route exact path="/">
-            <Main />
-          </Route>
           <Route exact path="/login/redirect">
             <LoginRedirect />
           </Route>
@@ -36,6 +30,12 @@ const App: React.FC = () => {
           <Route path="/login">
             <Login />
           </Route>
+          <AdminAuthedRoute path="/admin">
+            <AdminPage />
+          </AdminAuthedRoute>
+          <AuthedRoute path="/">
+            <Main />
+          </AuthedRoute>
         </Switch>
       </AppContainer>
       <BiseoToastContainer />
