bb0d656b46705e5df161308e16afef4aa3733a9f
kjy2844 <kjy2844@kaist.ac.kr>
2022-01-24 01:05:37 +0900

    feat(redux): get agenda from redux in user main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,19 +2,27 @@ import React, { useState, useEffect, useMemo } from 'react';
 import io from 'socket.io-client';
 import { Agenda } from '@/common/types';
 import { AgendaStatus } from '@/common/enums';
+import { useTypedSelector, useTypedDispatch } from '@/hooks';
 import ChatBox from '@/components/ChatBox';
 import UserAgenda from '@/components/UserAgenda';
 import { getToken } from '@/utils/auth';
 import axios from '@/utils/axios';
 import { UserMainContainer } from './styled';
 import { Redirect } from 'react-router-dom';
+import {
+  setAgendas,
+  addAgendas,
+  updateAgenda,
+  deleteAgenda,
+} from '@/store/slices/agendas';
 
 interface CommonMainProps {
   socket: SocketIOClient.Socket;
-  agendas: Agenda[];
 }
 
-const UserMain: React.FC<CommonMainProps> = ({ socket, agendas }) => {
+const UserMain: React.FC<CommonMainProps> = ({ socket }) => {
+  const agendas = useTypedSelector(state => state.agendas);
+
   return (
     <UserMainContainer>
       <div className="left">
@@ -32,7 +40,8 @@ const UserMain: React.FC<CommonMainProps> = ({ socket, agendas }) => {
 };
 
 const Main: React.FC = () => {
-  const [agendas, setAgendas] = useState<Agenda[]>([]);
+  const agendas = useTypedSelector(state => state.agendas);
+  const dispatch = useTypedDispatch();
   const [valid, setValid] = useState(true);
 
   const socket = useMemo(
@@ -53,7 +62,7 @@ const Main: React.FC = () => {
     async function getAgendas() {
       const { data } = await axios.get('/agendas').catch(() => ({ data: [] }));
       const agendas: Agenda[] = data.agendas ?? [];
-      setAgendas(agendas);
+      dispatch(setAgendas(agendas));
     }
 
     getAgendas();
@@ -65,54 +74,42 @@ const Main: React.FC = () => {
         ...payload,
         userChoice: null,
       };
-      setAgendas(prevState => [newAgenda, ...prevState]);
+      dispatch(addAgendas(newAgenda));
     });
 
     socket.on('agenda:started', (payload: Agenda) => {
-      setAgendas(agendas => {
-        return agendas.map(agenda => {
-          if (agenda._id === payload._id)
-            return { ...payload, userChoice: null };
-          else return agenda;
-        });
-      });
+      const newAgenda = {
+        ...payload,
+        userChoice: null,
+      };
+      dispatch(updateAgenda(newAgenda));
     });
 
     socket.on('agenda:terminated', (payload: Agenda) => {
-      setAgendas(agendas => {
-        return agendas.map(agenda => {
-          if (agenda._id === payload._id) return payload;
-          else return agenda;
-        });
-      });
+      dispatch(updateAgenda(payload));
     });
     // TODO cleanup (agendas)
   }, []);
 
   useEffect(() => {
     socket.on('agenda:edited', (payload: Agenda) => {
-      const newAgendas = agendas.map(agenda => {
-        if (agenda._id === payload._id) return { ...payload, userChoice: null };
-        else return agenda;
-      });
-      setAgendas(newAgendas);
+      const newAgenda = {
+        ...payload,
+        userChoice: null,
+      };
+      dispatch(updateAgenda(newAgenda));
     });
 
     socket.on('agenda:deleted', (payload: string) => {
-      const newAgendas = agendas.filter(agenda => agenda._id !== payload);
-      setAgendas(newAgendas);
+      dispatch(deleteAgenda(payload));
     });
   }, [agendas]);
-
-  const filteredAgendas = agendas.filter(
-    agenda => agenda.status !== AgendaStatus.PREPARE
-  );
 
   if (valid === false) return <Redirect to="/login" />;
 
   return (
     <div style={{ height: '100%' }}>
-      <UserMain socket={socket} agendas={filteredAgendas} />
+      <UserMain socket={socket} />
     </div>
   );
 };
