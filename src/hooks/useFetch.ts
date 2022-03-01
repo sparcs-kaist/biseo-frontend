e357f089632eb82779fe351e3820ac7f1b7fb8fd
kjy2844 <kjy2844@kaist.ac.kr>
2022-03-01 15:21:21 +0900

    fix(chat): fix duplicate chat

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,10 +2,11 @@ import { useState, useEffect, useCallback } from 'react';
 import axios from '@/utils/axios';
 import { MessageType } from '@/common/types';
 
-function useFetch(query: string, page: number) {
-  const [loading, setLoading] = useState(true);
+function useFetch(query: string) {
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   const sendQuery = useCallback(async () => {
     try {
@@ -17,16 +18,17 @@ function useFetch(query: string, page: number) {
         .catch(() => ({ data: [] }));
       const chatlogs: MessageType[] = data.chats ?? [];
 
+      setHasMore(chatlogs.length > 0);
       setList(prev => prev.concat(chatlogs));
       setLoading(false);
     } catch (err) {
       setError(err);
     }
-  }, [page]);
+  }, [query]);
 
   useEffect(() => {
-    sendQuery();
-  }, [query, sendQuery, page]);
+    if (hasMore) sendQuery();
+  }, [query]);
 
   return { loading, list, error };
 }
